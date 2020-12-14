e4bb220e15ff47efc1af94fc42b88f33e0a3f46f
yichenfromhyrule <jambako1995@gmail.com>
2020-12-13 23:41:37 -0500

    create a component

---
+++ components/BackgroundImg/BackgroundImg.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class BackgroundImg extends Component {
+  render(){
+      return(
+          <Text>
+              HIiiiii
+          </Text>
+      )
+  }
+}
