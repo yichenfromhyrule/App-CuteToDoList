4e16db17f6aaa110310a6564f1f05cbbb002600b
yichenfromhyrule <jambako1995@gmail.com>
2021-01-01 20:51:09 -0500

    Improve UI

--- components/HomePage/HomePageComponents/HomeStock/HomeStock.js
+++ components/HomePage/HomePageComponents/HomeStock/HomeStock.js
@@ -4,11 +4,39 @@ import { Button, Icon } from "react-native-elements";
 import styles from "./HomeStockStyle";
 
 export default function HomeStock(props) {
+  const [isLoading, setLoading] = React.useState(true);
+  const [data, setData] = React.useState('');
+  if(isLoading){
+    fetch("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol=TSLA", {
+	  "method": "GET",
+	  "headers": {
+	    "x-rapidapi-key": "6344837b96msh4dd4d2909be9cbbp1320a9jsn673352df54c6",
+		"x-rapidapi-host": "alpha-vantage.p.rapidapi.com"
+	  }
+    })
+    .then(response => response.json())
+    .then(data => {         
+      setData(data);
+      setLoading(false);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .finally(() => setLoading(false));;
+    }
+    const onePieceStockValues = Object.values(data);
+    let onePieceStock_name = "";
+    let onePieceStock_close = "";
+    if(onePieceStockValues[0]){
+        const onePieceStockArray = Object.entries(onePieceStockValues[0]); 
+        onePieceStock_name = onePieceStockArray[0][1];
+        onePieceStock_close = onePieceStockArray[7][1];
+  }
   return(
     <View>
         <View style={styles.stockContainer}>
             <View style={styles.stockNameContainer}>
-                <Text style={{color: 'white', fontSize: 24}}>{props.stockName}</Text>
+                <Text style={{color: 'white', fontSize: 24}}>{onePieceStock_name}</Text>
             </View>
             <View style={styles.stockTimeContainer}>
                 <Text style={{color: 'white', fontSize: 16}}>{props.highTime}</Text>
